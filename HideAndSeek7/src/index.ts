3c9b3eb324da59afda8230c44469354368765a6f
matt <matthew.mcmillian@ymail.com>
2023-07-10 17:04:44 -0400

    add map

--- HideAndSeek7/src/index.ts
+++ HideAndSeek7/src/index.ts
@@ -1,11 +1,11 @@
-import {AvatarShape, ColliderLayer, engine, GltfContainer, Transform} from '@dcl/sdk/ecs'
-import {Vector3} from '@dcl/sdk/math'
-import {bounceScalingSystem, circularSystem, spawnerSystem} from './systems'
+import { AvatarShape, ColliderLayer, engine, GltfContainer, Transform } from '@dcl/sdk/ecs'
+import { Quaternion, Vector3 } from '@dcl/sdk/math'
+import { bounceScalingSystem, circularSystem, spawnerSystem } from './systems'
 
-import {setupUi} from './ui'
-import {followSystem} from "./systems/followSystem";
-import {ShootingSystem} from "./systems/shootingSystem";
-import {initGamePlay} from "./colyseus/gameplay";
+import { setupUi } from './ui'
+import { followSystem } from './systems/followSystem'
+import { ShootingSystem } from './systems/shootingSystem'
+import { initGamePlay } from './colyseus/gameplay'
 
 // Defining behavior. See `src/systems.ts` file.
 engine.addSystem(circularSystem)
@@ -17,43 +17,51 @@ export * from '@dcl/sdk'
 import './polyfill/delcares'
 
 export function main() {
-    setupUi()
+  setupUi()
 
-    initGamePlay()
+  initGamePlay()
 
+  const dreamForest = engine.addEntity() //new Entity('entity')
+  GltfContainer.create(dreamForest, {
+    invisibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS,
+    src: 'models/LucidDreams_Map.glb'
+  })
 
-    const myAvatar = engine.addEntity()
-    AvatarShape.create(myAvatar, {
-        id: "",
-        emotes: ['urn:decentraland:matic:collections-v2:0xa80aea22d0fe9d34ca72ce304ef427bbefee1f11:2'],
-        wearables: [],
-        expressionTriggerId: "urn:decentraland:matic:collections-v2:0xa80aea22d0fe9d34ca72ce304ef427bbefee1f11:2",
-        expressionTriggerTimestamp: Math.round(+new Date() / 1000)
-    })
+  Transform.create(dreamForest, {
+    position: Vector3.create(24, 0, 24),
+    rotation: Quaternion.create(0, 0, 0, 1),
+    scale: Vector3.create(0.999, 0.999, 0.999)
+  })
 
-    AvatarShape.getMutable(myAvatar).expressionTriggerId =  "urn:decentraland:matic:collections-v2:0xa80aea22d0fe9d34ca72ce304ef427bbefee1f11:2"
-    AvatarShape.getMutable(myAvatar).expressionTriggerTimestamp =  Math.round((+new Date() / 1000 )+ 20)
+  const myAvatar = engine.addEntity()
+  AvatarShape.create(myAvatar, {
+    id: '',
+    emotes: ['urn:decentraland:matic:collections-v2:0xa80aea22d0fe9d34ca72ce304ef427bbefee1f11:2'],
+    wearables: [],
+    expressionTriggerId: 'urn:decentraland:matic:collections-v2:0xa80aea22d0fe9d34ca72ce304ef427bbefee1f11:2',
+    expressionTriggerTimestamp: Math.round(+new Date() / 1000)
+  })
 
-    Transform.create(myAvatar, {
-        position: Vector3.create(12, 0.25, 12)
-    })
+  AvatarShape.getMutable(myAvatar).expressionTriggerId =
+    'urn:decentraland:matic:collections-v2:0xa80aea22d0fe9d34ca72ce304ef427bbefee1f11:2'
+  AvatarShape.getMutable(myAvatar).expressionTriggerTimestamp = Math.round(+new Date() / 1000 + 20)
 
-    //
-    // const zombiehouse = engine.addEntity()
-    // Transform.create(zombiehouse, {
-    //     position: Vector3.create(8, 0.01, 13.2),
-    //     scale: Vector3.create(1, 1, 1),
-    // });
-    // GltfContainer.create(zombiehouse, {
-    //         src: "models/Zombiehouse.glb",
-    //         invisibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS,
-    //     }
-    // )
+  Transform.create(myAvatar, {
+    position: Vector3.create(12, 0.25, 12)
+  })
 
+  //
+  // const zombiehouse = engine.addEntity()
+  // Transform.create(zombiehouse, {
+  //     position: Vector3.create(8, 0.01, 13.2),
+  //     scale: Vector3.create(1, 1, 1),
+  // });
+  // GltfContainer.create(zombiehouse, {
+  //         src: "models/Zombiehouse.glb",
+  //         invisibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS,
+  //     }
+  // )
 
-
-    engine.addSystem(followSystem)
-    engine.addSystem(ShootingSystem)
-
-
+  engine.addSystem(followSystem)
+  engine.addSystem(ShootingSystem)
 }
--- HideAndSeek7/src/zombies/zombie.ts
+++ HideAndSeek7/src/zombies/zombie.ts
@@ -1,153 +1,141 @@
 import {
-    Animator,
-    ColliderLayer,
-    engine,
-    Entity,
-    GltfContainer,
-    InputAction,
-    pointerEventsSystem,
-    removeEntityWithChildren,
-    Schemas,
-    TextShape,
-    Transform,
-    TransformType
-} from "@dcl/sdk/ecs";
-import {activeZombies, connectedRoom} from "../colyseus/gameplay";
-
+  Animator,
+  ColliderLayer,
+  engine,
+  Entity,
+  GltfContainer,
+  InputAction,
+  pointerEventsSystem,
+  removeEntityWithChildren,
+  Schemas,
+  TextShape,
+  Transform,
+  TransformType
+} from '@dcl/sdk/ecs'
+import { activeZombies, connectedRoom } from '../colyseus/gameplay'
 
 export class Character {
-    entity: Entity
+  entity: Entity
 
-    constructor(modelSrc: string, transform: TransformType) {
+  constructor(modelSrc: string, transform: TransformType) {
+    console.log('adding character', modelSrc, transform)
+    const charEnt = engine.addEntity()
+    GltfContainer.create(charEnt, {
+      src: modelSrc,
+      invisibleMeshesCollisionMask: undefined,
+      visibleMeshesCollisionMask: ColliderLayer.CL_POINTER | ColliderLayer.CL_PHYSICS
+    })
+    Transform.create(charEnt, transform)
 
-        console.log("adding character", modelSrc, transform);
-        const charEnt = engine.addEntity()
-        GltfContainer.create(charEnt, {
-            src: modelSrc,
-            invisibleMeshesCollisionMask: undefined,
-            visibleMeshesCollisionMask: ColliderLayer.CL_POINTER | ColliderLayer.CL_PHYSICS,
-        })
-        Transform.create(charEnt, transform)
+    Animator.create(charEnt, {
+      states: [
+        {
+          name: 'run',
+          clip: 'run',
+          playing: true,
+          loop: true,
+          speed: 1,
+          shouldReset: false
+        },
+        {
+          name: 'attack',
+          clip: 'attack',
+          loop: true,
+          speed: 1,
+          shouldReset: false
+        }
+      ]
+    })
 
-        Animator.create(charEnt, {
-            states: [
-                {
-                    name: "run",
-                    clip: "run",
-                    playing: true,
-                    loop: true,
-                    speed: 1,
-                    shouldReset: false
-                },
-                {
-                    name: "attack",
-                    clip: "attack",
-                    loop: true,
-                    speed: 1,
-                    shouldReset: false
-                }
-            ]
-        })
+    //Animator.playSingleAnimation(charEnt, "run")
 
-        //Animator.playSingleAnimation(charEnt, "run")
-
-        this.entity = charEnt
-
-    }
-
+    this.entity = charEnt
+  }
 }
 
-const zombieDefaults = {moveSpeed: 1, attackDistance: 2, attackTimer: 1.5, attackTime: 1.5}
-export const ZombieC = engine.defineComponent("Zombie", {
+const zombieDefaults = { moveSpeed: 1, attackDistance: 2, attackTimer: 1.5, attackTime: 1.5 }
+export const ZombieC = engine.defineComponent(
+  'Zombie',
+  {
     moveSpeed: Schemas.Number,
     attackDistance: Schemas.Number,
     attackTimer: Schemas.Number,
     attackTime: Schemas.Number
-}, zombieDefaults)
-
+  },
+  zombieDefaults
+)
 
 export class Zombie extends Character {
-    health: number;
-    serverId: string;
+  health: number
+  serverId: string
 
-    constructor(transform: TransformType, serverId: string) {
-        super('models/zombie.glb', transform);
-        this.health = 100
-        this.serverId = serverId
-        ZombieC.create(this.entity)
+  constructor(transform: TransformType, serverId: string) {
+    super('models/ghost.glb', transform)
+    this.health = 100
+    this.serverId = serverId
+    ZombieC.create(this.entity)
 
-        TextShape.create(this.entity,{
-            text: ''+this.health
-        })
+    TextShape.create(this.entity, {
+      text: '' + this.health
+    })
 
+    pointerEventsSystem.onPointerDown(
+      {
+        entity: this.entity,
+        opts: { button: InputAction.IA_POINTER, hoverText: 'Shoot' }
+      },
+      function (cmd) {
+        console.log('clicked', cmd.hit)
 
-        pointerEventsSystem.onPointerDown(
-            {
-                entity: this.entity,
-                opts: { button: InputAction.IA_POINTER, hoverText: 'Shoot' },
-            },
-            function (cmd) {
-                console.log('clicked', cmd.hit)
+        connectedRoom.send('damage-enemy', serverId)
+      }
+    )
+  }
 
-                connectedRoom.send("damage-enemy", serverId)
+  damage(damage: number) {
+    this.health -= damage
+    TextShape.getMutable(this.entity).text = '' + this.health
+  }
 
-            }
-        )
+  attack() {
+    Animator.playSingleAnimation(this.entity, 'attack')
+  }
 
+  hit(damage: number) {
+    this.health -= damage
 
+    console.log(this.health)
+    if (this.health <= 0) {
+      console.log('die')
+      this.die()
     }
+  }
 
-    damage(damage: number) {
-        this.health -= damage;
-        TextShape.getMutable(this.entity).text = ''+this.health
-    }
+  die() {
+    console.log('zombie died')
+    removeEntityWithChildren(engine, this.entity)
+    engine.removeEntity(this.entity)
+    activeZombies.delete(this.serverId)
+  }
 
-    attack() {
-        Animator.playSingleAnimation(this.entity, "attack")
-    }
+  // Play walking animation
+  walk() {
+    Animator.playSingleAnimation(this.entity, 'run')
+  }
 
-    hit(damage: number) {
-        this.health -= damage;
-
-        console.log(this.health)
-        if(this.health <= 0){
-
-            console.log("die")
-            this.die()
-        }
-    }
-
-    die(){
-        console.log("zombie died")
-        removeEntityWithChildren(engine, this.entity)
-        engine.removeEntity(this.entity)
-        activeZombies.delete(this.serverId)
-    }
-
-    // Play walking animation
-    walk() {
-        Animator.playSingleAnimation(this.entity, "run")
-    }
-
-    // Bug workaround: otherwise the next animation clip won't play
-    stopAnimations() {
-        Animator.stopAllAnimations(this.entity)
-    }
+  // Bug workaround: otherwise the next animation clip won't play
+  stopAnimations() {
+    Animator.stopAllAnimations(this.entity)
+  }
 }
 
 export const playSingleAnimForEntity = (entity: Entity, animName: string) => {
+  console.log('play')
+  Animator.playSingleAnimation(entity, animName)
 
-    console.log("play")
-    Animator.playSingleAnimation(entity, animName)
-
-
-    // console.log("playing", Animator.getClip(entity, animName).playing )
-    //
-    // if (Animator.getClip(entity, animName).playing == false) {
-    //
-    // }
+  // console.log("playing", Animator.getClip(entity, animName).playing )
+  //
+  // if (Animator.getClip(entity, animName).playing == false) {
+  //
+  // }
 }
-
-
-
-
