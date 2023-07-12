70a7fbd07b86763eb6fd4e79b131e9b062e4e4c4
matt <matthew.mcmillian@ymail.com>
2023-07-12 18:52:24 -0400

    add dialog

--- HideAndSeek7/src/index.ts
+++ HideAndSeek7/src/index.ts
@@ -1,223 +1,193 @@
 import {
-    ColliderLayer,
-    engine,
-    Entity,
-    GltfContainer,
-    Material,
-    MeshRenderer,
-    TextureFilterMode,
-    TextureWrapMode,
-    Transform
+  ColliderLayer,
+  engine,
+  Entity,
+  GltfContainer,
+  Material,
+  MeshRenderer,
+  TextureFilterMode,
+  TextureWrapMode,
+  Transform
 } from '@dcl/sdk/ecs'
-import {Color4, Quaternion, Vector3} from '@dcl/sdk/math'
+import { Color4, Quaternion, Vector3 } from '@dcl/sdk/math'
 
-import {setupUi} from './ui'
-import {followSystem} from "./systems/followSystem";
-import {initGamePlay} from "./colyseus/gameplay";
+import { setupUi } from './ui'
+import { followSystem } from './systems/followSystem'
+import { initGamePlay } from './colyseus/gameplay'
 import './polyfill/delcares'
-import {initBeacons} from "./beacons/beacon";
-import {setupWeapons} from "./weapon";
-import *  as  npc from 'dcl-npc-toolkit'
-import {Dialog} from "dcl-npc-toolkit";
+import { initBeacons } from './beacons/beacon'
+import { setupWeapons } from './weapon'
+import * as npc from 'dcl-npc-toolkit'
+import { Dialog } from 'dcl-npc-toolkit'
 
 export * from '@dcl/sdk'
 
-
 export const wallTexture = Material.Texture.Common({
-    src: 'images/digital_painting_galaxy_lucid_dream_sky_trees__3.jpg',
-    filterMode: TextureFilterMode.TFM_BILINEAR,
-    wrapMode: TextureWrapMode.TWM_REPEAT
+  src: 'images/digital_painting_galaxy_lucid_dream_sky_trees__3.jpg',
+  filterMode: TextureFilterMode.TFM_BILINEAR,
+  wrapMode: TextureWrapMode.TWM_REPEAT
 })
 
 export const wallTextureLight = Material.Texture.Common({
-    src: 'images/digital_painting_galaxy_lucid_dream_sky_trees.jpg',
-    filterMode: TextureFilterMode.TFM_BILINEAR,
-    wrapMode: TextureWrapMode.TWM_REPEAT
+  src: 'images/digital_painting_galaxy_lucid_dream_sky_trees.jpg',
+  filterMode: TextureFilterMode.TFM_BILINEAR,
+  wrapMode: TextureWrapMode.TWM_REPEAT
 })
 
-
-export const wallComponent = engine.defineComponent("wallComponent", {})
+export const wallComponent = engine.defineComponent('wallComponent', {})
 
 export let dreamForestDark: Entity
 export let dreamForestLight: Entity
 
 export function main() {
-    setupUi()
+  setupUi()
 
-    initGamePlay()
-    initBeacons()
+  initGamePlay()
+  initBeacons()
 
-    setupWeapons()
+  setupWeapons()
 
-    dreamForestDark = engine.addEntity()
-    GltfContainer.create(dreamForestDark, {
-        invisibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS,
-        src: 'models/LucidDreams_Map.glb',
-    })
-    Transform.create(dreamForestDark, {
-        position: Vector3.create(24, 0, 24),
-        rotation: Quaternion.create(0, 0, 0, 1),
-        scale: Vector3.create(0.999, 0.999, 0.999),
-    })
+  dreamForestDark = engine.addEntity()
+  GltfContainer.create(dreamForestDark, {
+    invisibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS,
+    src: 'models/LucidDreams_Map.glb'
+  })
+  Transform.create(dreamForestDark, {
+    position: Vector3.create(24, 0, 24),
+    rotation: Quaternion.create(0, 0, 0, 1),
+    scale: Vector3.create(0.999, 0.999, 0.999)
+  })
 
-    dreamForestLight = engine.addEntity()
-    GltfContainer.create(dreamForestLight, {
-        invisibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS,
-        src: 'models/Light_LucidDreams_Map.glb',
-    })
-    Transform.create(dreamForestLight, {
-        position: Vector3.create(24, 0, 24),
-        rotation: Quaternion.create(0, 0, 0, 1),
-        scale: Vector3.create(0, 0, 0),
-    })
+  dreamForestLight = engine.addEntity()
+  GltfContainer.create(dreamForestLight, {
+    invisibleMeshesCollisionMask: ColliderLayer.CL_PHYSICS,
+    src: 'models/Light_LucidDreams_Map.glb'
+  })
+  Transform.create(dreamForestLight, {
+    position: Vector3.create(24, 0, 24),
+    rotation: Quaternion.create(0, 0, 0, 1),
+    scale: Vector3.create(0, 0, 0)
+  })
 
+  const wall1 = engine.addEntity()
+  Material.setPbrMaterial(wall1, {
+    texture: wallTexture,
+    emissiveTexture: wallTexture,
+    emissiveColor: Color4.White()
+  })
+  MeshRenderer.setPlane(wall1, [0, 0.5, 0, 1, 0.5, 1, 0.5, 0.5, 0, 0.5, 0, 1, 0.5, 1, 0.5, 0.5])
+  Transform.create(wall1, {
+    position: Vector3.create(24, 12, 0),
+    scale: Vector3.create(48, 24, 48),
+    rotation: Quaternion.fromEulerDegrees(0, 0, 0)
+  })
+  wallComponent.create(wall1)
 
-    const wall1 = engine.addEntity()
-    Material.setPbrMaterial(wall1, {
-        texture: wallTexture,
-        emissiveTexture: wallTexture,
-        emissiveColor: Color4.White()
-    })
-    MeshRenderer.setPlane(wall1, [
-        0, .5,
-        0, 1,
-        .5, 1,
-        .5, .5,
-        0, .5,
-        0, 1,
-        .5, 1,
-        .5, .5,
-    ])
-    Transform.create(wall1, {
-        position: Vector3.create(24, 12, 0),
-        scale: Vector3.create(48, 24, 48),
-        rotation: Quaternion.fromEulerDegrees(0, 0, 0)
-    })
-    wallComponent.create(wall1)
+  const wall2 = engine.addEntity()
+  Material.setPbrMaterial(wall2, {
+    texture: wallTexture,
+    emissiveTexture: wallTexture,
+    emissiveColor: Color4.White()
+  })
 
-    const wall2 = engine.addEntity()
-    Material.setPbrMaterial(wall2, {
-        texture: wallTexture,
-        emissiveTexture: wallTexture,
-        emissiveColor: Color4.White()
-    })
+  MeshRenderer.setPlane(wall2, [0.5, 0.5, 0.5, 1, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 0.5])
+  Transform.create(wall2, {
+    position: Vector3.create(24, 12, 48),
+    scale: Vector3.create(48, 24, 48),
+    rotation: Quaternion.fromEulerDegrees(0, 0, 0)
+  })
+  wallComponent.create(wall2)
 
-    MeshRenderer.setPlane(wall2, [
-        .5, .5,
-        .5, 1,
-        1, 1,
-        1, .5,
-        .5, .5,
-        .5, 1,
-        1, 1,
-        1, .5,
-    ])
-    Transform.create(wall2, {
-        position: Vector3.create(24, 12, 48),
-        scale: Vector3.create(48, 24, 48),
-        rotation: Quaternion.fromEulerDegrees(0, 0, 0)
-    })
-    wallComponent.create(wall2)
+  const wall3 = engine.addEntity()
+  Material.setPbrMaterial(wall3, {
+    texture: wallTexture,
+    emissiveTexture: wallTexture,
+    emissiveColor: Color4.White()
+  })
+  MeshRenderer.setPlane(wall3, [0, 0, 0, 1, 0.5, 1, 0.5, 0, 0, 0, 0, 1, 0.5, 1, 0.5, 0])
+  Transform.create(wall3, {
+    position: Vector3.create(0, 12, 24),
+    scale: Vector3.create(48, 24, 48),
+    rotation: Quaternion.fromEulerDegrees(0, 90, 0)
+  })
+  wallComponent.create(wall3)
 
-    const wall3 = engine.addEntity()
-    Material.setPbrMaterial(wall3, {
-        texture: wallTexture,
-        emissiveTexture: wallTexture,
-        emissiveColor: Color4.White()
-    })
-    MeshRenderer.setPlane(wall3, [
-        0, 0,
-        0, 1,
-        .5, 1,
-        .5, 0,
-        0, 0,
-        0, 1,
-        .5, 1,
-        .5, 0,
-    ])
-    Transform.create(wall3, {
-        position: Vector3.create(0, 12, 24),
-        scale: Vector3.create(48, 24, 48),
-        rotation: Quaternion.fromEulerDegrees(0, 90, 0)
-    })
-    wallComponent.create(wall3)
+  const wall4 = engine.addEntity()
+  Material.setPbrMaterial(wall4, {
+    texture: wallTexture,
+    emissiveTexture: wallTexture,
+    emissiveColor: Color4.White()
+  })
+  MeshRenderer.setPlane(wall4, [0.5, 0, 0.5, 1, 1, 1, 1, 0, 5, 0, 0.5, 1, 1, 1, 1, 0])
+  Transform.create(wall4, {
+    position: Vector3.create(48, 12, 24),
+    scale: Vector3.create(48, 24, 48),
+    rotation: Quaternion.fromEulerDegrees(0, 90, 0)
+  })
+  wallComponent.create(wall4)
 
+  // MeshRenderer.setPlane(wall2, [
+  //         .5, 0,
+  //         .5, 1,
+  //     1, 1,
+  //     1, .5,
+  //
+  //     0, 0,
+  //     0, 1,
+  //     .5, 1,
+  //     .5, 0,
+  // ])
 
-    const wall4 = engine.addEntity()
-    Material.setPbrMaterial(wall4, {
-        texture: wallTexture,
-        emissiveTexture: wallTexture,
-        emissiveColor: Color4.White()
-    })
-    MeshRenderer.setPlane(wall4, [
-        .5, 0,
-        .5, 1,
-        1, 1,
-        1, 0, 5, 0,
-        .5, 1,
-        1, 1,
-        1, 0,
-    ])
-    Transform.create(wall4, {
-        position: Vector3.create(48, 12, 24),
-        scale: Vector3.create(48, 24, 48),
-        rotation: Quaternion.fromEulerDegrees(0, 90, 0)
-    })
-    wallComponent.create(wall4)
+  let ILoveCats: Dialog[] = [
+    {
+      text: `Oh... um, hello there. I'm Sarah. I... I'm feeling really sad because I'm stuck in this scary nightmare. Can you help me, please? I'll explain how to get out, okay?`
+    },
+    {
+      text: `To leave this place you must follow these steps: 
+            \n1. Defeat the ghost and collect energy.`
+    },
+    {
+      text: `\n2. Power up the dream beacons using the energy. 
+             \n3. Protect the bed beacon from power loss.`
+    },
+    {
+      text: `\nYour goal is to power up all the dream beacons to release the nightmare.`
+    },
+    {
+      text: ` Coordinate with friends to strategically manage energy and explore for hidden energy boosts!`
+    },
+    {
+      text: `I believe in you! Let's beat this nightmare together. You've got this!`,
+      isEndOfDialog: true
+    }
+  ]
 
+  let myNPC = npc.create(
+    {
+      position: Vector3.create(17, 0, 34),
+      rotation: Quaternion.fromEulerDegrees(0, 25, 0),
+      scale: Vector3.create(1, 1, 1)
+    },
+    //NPC Data Object
+    {
+      type: npc.NPCType.CUSTOM,
+      model: 'models/GirlLP.glb',
+      reactDistance: 3,
+      onlyClickTrigger: true,
+      onlyETrigger: true,
+      textBubble: true,
+      idleAnim: 'cry',
+      bubbleXOffset: 0.75,
+      bubbleYOffset: -0.4,
 
-    // MeshRenderer.setPlane(wall2, [
-    //         .5, 0,
-    //         .5, 1,
-    //     1, 1,
-    //     1, .5,
-    //
-    //     0, 0,
-    //     0, 1,
-    //     .5, 1,
-    //     .5, 0,
-    // ])
+      onActivate: () => {
+        console.log('npc activated')
+        //npc.talk(myNPC, ILoveCats)
 
-    let ILoveCats: Dialog[] = [
-        {
-            text: `Hi there! I'm Sarah, a sad little girl trapped in this nightmare. I need your help to escape. Listen closely, okay?`,
-        },
-        {
-            text: `Once upon a time, I was a happy girl with dreams. But a terrible curse brought me here. To set me free, you must defeat the ghost and collect energy.`,
-        },
-        {
-            text: `Use that energy to power up the dream beacons and protect the bed beacon at all costs. If the bed beacon loses power, the nightmare wins, and I'll remain trapped forever.`,
-            isEndOfDialog: true
-        }
-    ]
+        npc.talkBubble(myNPC, ILoveCats)
+      }
+    }
+  )
 
-    let myNPC = npc.create(
-        {
-            position: Vector3.create(17, 0, 34),
-            rotation: Quaternion.fromEulerDegrees(0, 25, 0),
-            scale: Vector3.create(1, 1, 1)
-        },
-        //NPC Data Object
-        {
-            type: npc.NPCType.CUSTOM,
-            model: 'models/GirlLP.glb',
-            reactDistance: 3,
-            onlyClickTrigger: true,
-            onlyETrigger: true,
-            textBubble: true,
-            idleAnim: 'cry',
-            bubbleXOffset: .75,
-            bubbleYOffset: -.4,
-
-            onActivate: () => {
-                console.log('npc activated');
-                //npc.talk(myNPC, ILoveCats)
-
-                npc.talkBubble(myNPC, ILoveCats)
-            }
-        }
-    )
-
-
-    engine.addSystem(followSystem)
+  engine.addSystem(followSystem)
 }
